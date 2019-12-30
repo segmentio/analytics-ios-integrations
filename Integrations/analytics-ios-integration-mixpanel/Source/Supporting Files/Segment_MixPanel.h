c5237ac4c60759c687335a750de89516e9c5bbc5
Olla Ashour <olla.ashour@andela.com>
2019-12-31 00:30:05 +0200

    DAND-45-Add mixpanel logic and tests to dynamic framework in "analytics-ios-integration-mixpanel" integrations folder

---
+++ Integrations/analytics-ios-integration-mixpanel/Source/Supporting Files/Segment_MixPanel.h
@@ -0,0 +1,19 @@
+//
+//  Segment_MixPanel.h
+//  Segment-MixPanel
+//
+//  Created by Olla Ashour on 12/31/19.
+//  Copyright © 2019 Segment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segment_MixPanel.
+FOUNDATION_EXPORT double Segment_MixPanelVersionNumber;
+
+//! Project version string for Segment_MixPanel.
+FOUNDATION_EXPORT const unsigned char Segment_MixPanelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_MixPanel/PublicHeader.h>
+
+
