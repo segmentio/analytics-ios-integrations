f17b0ebc9933369d00d5da9f0f5f78d4907897f1
Olla Ashour <olla.ashour@andela.com>
2019-12-31 00:56:34 +0200

    DAND-45-Rename project to "...Mixpanel" instead of "..MixPanel"

--- Integrations/analytics-ios-integration-mixpanel/Source/Supporting Files/Segment_MixPanel.h
+++ Integrations/analytics-ios-integration-mixpanel/Source/Supporting Files/Segment_MixPanel.h
@@ -1,6 +1,6 @@
 //
-//  Segment_MixPanel.h
-//  Segment-MixPanel
+//  Segment_Mixpanel.h
+//  Segment-Mixpanel
 //
 //  Created by Olla Ashour on 12/31/19.
 //  Copyright © 2019 Segment. All rights reserved.
@@ -8,12 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Segment_MixPanel.
-FOUNDATION_EXPORT double Segment_MixPanelVersionNumber;
+//! Project version number for Segment_Mixpanel.
+FOUNDATION_EXPORT double Segment_MixpanelVersionNumber;
 
-//! Project version string for Segment_MixPanel.
-FOUNDATION_EXPORT const unsigned char Segment_MixPanelVersionString[];
+//! Project version string for Segment_Mixpanel.
+FOUNDATION_EXPORT const unsigned char Segment_MixpanelVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Segment_MixPanel/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_Mixpanel/PublicHeader.h>
 
 
