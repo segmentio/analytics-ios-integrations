fdddd74376baea077ffa5a9dffa50f83ce3cd185
Olla Ashour <olla.ashour@andela.com>
2020-01-13 19:13:56 +0200

    DAND-43-Add clang format rules, and for formatting objective c code.

--- Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegrationFactory.h
+++ Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegrationFactory.h
@@ -1,5 +1,5 @@
-#import <Foundation/Foundation.h>
 #import <Analytics/SEGIntegrationFactory.h>
+#import <Foundation/Foundation.h>
 
 @interface SEGFacebookAppEventsIntegrationFactory : NSObject<SEGIntegrationFactory>
 
