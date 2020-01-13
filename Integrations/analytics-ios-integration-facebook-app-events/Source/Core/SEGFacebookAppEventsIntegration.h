4b633afaea86336acc2ce0d654c18356251a41c8
Olla Ashour <olla.ashour@andela.com>
2020-01-13 19:13:56 +0200

    DAND-43-Add clang format rules, and for formatting objective c code.
    (cherry picked from commit fdddd74376baea077ffa5a9dffa50f83ce3cd185)

--- Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegration.h
+++ Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegration.h
@@ -1,9 +1,9 @@
-#import <Foundation/Foundation.h>
 #import <Analytics/SEGIntegration.h>
+#import <Foundation/Foundation.h>
 
 @interface SEGFacebookAppEventsIntegration : NSObject<SEGIntegration>
 
-@property(nonatomic, strong) NSDictionary *settings;
+@property (nonatomic, strong) NSDictionary *settings;
 
 - (id)initWithSettings:(NSDictionary *)settings;
 
--- Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegrationFactory.m
+++ Integrations/analytics-ios-integration-facebook-app-events/Source/Core/SEGFacebookAppEventsIntegrationFactory.m
@@ -3,29 +3,25 @@
 
 @implementation SEGFacebookAppEventsIntegrationFactory
 
-+ (instancetype)instance
-{
++ (instancetype)instance {
     static dispatch_once_t once;
     static SEGFacebookAppEventsIntegrationFactory *sharedInstance;
     dispatch_once(&once, ^{
-        sharedInstance = [[self alloc] init];
+      sharedInstance = [[self alloc] init];
     });
     return sharedInstance;
 }
 
-- (id)init
-{
+- (id)init {
     self = [super init];
     return self;
 }
 
-- (id<SEGIntegration>)createWithSettings:(NSDictionary *)settings forAnalytics:(SEGAnalytics *)analytics
-{
+- (id<SEGIntegration>)createWithSettings:(NSDictionary *)settings forAnalytics:(SEGAnalytics *)analytics {
     return [[SEGFacebookAppEventsIntegration alloc] initWithSettings:settings];
 }
 
-- (NSString *)key
-{
+- (NSString *)key {
     return @"Facebook App Events";
 }
 
