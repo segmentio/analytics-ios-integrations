6d1658c496219cccf00419a4414b461f7d3a5d04
Olla Ashour <olla.ashour@andela.com>
2020-01-13 19:14:36 +0200

    DAND-43-Update scripts to be more user friendly and print status.
    (cherry picked from commit 2ac07da21aa55fd63ec962545f3cd32da4064999)

--- Check.sh
+++ Check.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
 
+# Constants for colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+
 # Install dependencies from gem file to maintain consistency in all environments
-echo "Checking if gem 'Bundler', exists otherwise install"
+echo "$(tput setaf 4)Checking if gem 'Bundler' exists"
+
 which -s bundler
     
 if [[ $? != 0 ]] ; then
+  echo "$(tput setaf 3) Installing 'Bundler'..."
+
   sudo gem install bundler
   bundle init
 fi
 
+echo "$(tput setaf 3)Bundler installed...Installing/updating required gems"
 bundle install
 
-echo "Checking if 'oclint' exists, otherwise install"
+echo "$(tput setaf 4)Checking if 'oclint' exists.."
+
 # Install oclint 
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
+    echo "$(tput setaf 1)Homebrew not installed, installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
@@ -23,12 +37,15 @@ else
     which -s oclint
     
     if [[ $? != 0 ]] ; then
-   	 # Install oclint using homebrew
+
+   	# Install oclint using homebrew
+	
+	echo "$(tput setaf 3)Installing OClint"
 
     	brew tap oclint/formulae
  	brew install oclint
     else
+        echo "$(tput setaf 3)Updating OClint..."
         brew upgrade oclint
     fi
 fi
-
