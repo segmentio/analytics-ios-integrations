2ac07da21aa55fd63ec962545f3cd32da4064999
Olla Ashour <olla.ashour@andela.com>
2020-01-13 19:14:36 +0200

    DAND-43-Update scripts to be more user friendly and print status.

--- Lint.sh
+++ Lint.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+
+# Run oclint related commands to check for any issues
+
 unset LLVM_TARGET_TRIPLE_SUFFIX
 
 xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook clean
 xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook COMPILER_INDEX_STORE_ENABLE=NO | xcpretty -r json-compilation-database --output compile_commands.json
 oclint-json-compilation-database —e Segment_Facebook.h Segment_Facebook_vers.c -- build -report-type xcode
 
-
+# Run pod lint to check for any issues in podspec
 pod lib lint --use-libraries --allow-warnings
