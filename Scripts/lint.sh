cb526cff46508e6d45194ccfa4a3f4a28511b1e3
Olla Ashour <olla.ashour@andela.com>
2020-01-14 02:46:04 +0200

    DAND-43-Update OCLint exclude checks to include Pods directory

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -20,7 +20,7 @@ unset LLVM_TARGET_TRIPLE_SUFFIX
 
 xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook clean
 xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook COMPILER_INDEX_STORE_ENABLE=NO | xcpretty -r json-compilation-database --output compile_commands.json
-oclint-json-compilation-database —e Segment_Facebook.h Segment_Facebook_vers.c -- build -report-type xcode
+oclint-json-compilation-database —e Segment_Facebook.h Segment_Facebook_vers.c Pods -- build -report-type xcode
 
 echo "$(tput setaf 4) Linting pod spec"
 
