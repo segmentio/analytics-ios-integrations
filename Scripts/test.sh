5ffc3988217e0df800df4e11aaca19d087502438
Olla Ashour <olla.ashour@andela.com>
2020-01-14 10:07:12 +0200

    DAND-43- Add tests checks for mixpanel and firebase targets

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -4,4 +4,6 @@
 cd ..
 
 # Check if tests run successfully
-xcodebuild test -target TARGET -configuration Debug -scheme Segment-Facebook
+xcodebuild test -target TARGET -configuration Debug -scheme Segment-Facebook | xcpretty
+xcodebuild test -target TARGET -configuration Debug -scheme Segment-Mixpanel | xcpretty
+xcodebuild test -target TARGET -configuration Debug -scheme Segment-Firebase | xcpretty
