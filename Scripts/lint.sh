69ae9d430314a2413430f5fab7bf45e082c5dc60
Olla Ashour <olla.ashour@andela.com>
2020-01-16 21:54:05 +0200

    DAND-43-Update lint script to take Targets dynamically and also directory paths of destinations

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-# Move to parent directory to lint
+# Move to root, to run oclint on workspace
 cd ..
 
 # Run oclint related commands to check for any issues
+SCHEME=${1?Please Provide Target Name}
 
 echo "$(tput setaf 4) Running OClint"
 
 unset LLVM_TARGET_TRIPLE_SUFFIX
 
-xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook clean
-xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook COMPILER_INDEX_STORE_ENABLE=NO | xcpretty -r json-compilation-database --output compile_commands.json
-oclint-json-compilation-database —e Segment_Facebook.h Segment_Facebook_vers.c Pods -- build -report-type xcode
+xcodebuild -target $SCHEME -configuration Debug -scheme $SCHEME clean
+xcodebuild -target $SCHEME -configuration Debug -scheme $SCHEME COMPILER_INDEX_STORE_ENABLE=NO | xcpretty -r json-compilation-database --output compile_commands.json
+oclint-json-compilation-database -e Pods -e Xcode/DerivedData -- -report-type xcode
 
-echo "$(tput setaf 4) Linting pod spec"
-
-# Move to integrations destinations to run pod lint
-cd Integrations/analytics-ios-integration-facebook-app-events
 
 # Run pod lint to check for any issues in podspec
+
+echo "$(tput setaf 4) Linting pod spec"
+read -p 'Please Enter Directory of Intergation to Lint Pod Spec: ' dir
+
+# Move to integrations destinations to run pod lint
+cd $dir
+
 pod lib lint --use-libraries --allow-warnings
